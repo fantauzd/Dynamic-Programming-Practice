62b8003fbed0a202a44be4e4237a871318973b38
fantauzd <fantauzd@oregonstate.edu>
2024-04-20 16:59:44 -0400

    new version that replaces global variable with additional parameter.

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,14 +1,15 @@
 # Dynamic Programming practice
 
 FibMemo = {0:1, 1:1}
-def Fib_top_down(n):
-    print("Calculate for n: %s, FibMemo= %s " % (n, FibMemo))
-    if n in FibMemo:
-        print("Found for n: %s, in FibMemo= %s " % (n, FibMemo))
-        return FibMemo[n]
-    FibMemo[n] = Fib_top_down(n-1) + Fib_top_down(n-2)
+def Fib_top_down(n, fibmemo=None):
+    print("Calculate for n: %s, FibMemo= %s " % (n, fibmemo))
+    if fibmemo is None:
+        fibmemo = {0:1, 1:1}
+    if n not in fibmemo:
+        fibmemo[n] = Fib_top_down(n-1, fibmemo) + Fib_top_down(n-2, fibmemo)
+
     print("Returning Answer for n: %s" %(n))
-    return FibMemo[n]
+    return fibmemo[n]
 
 if __name__ == "__main__":
     print(Fib_top_down(5))
