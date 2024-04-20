c41a40730f5282ce9c3b2d1faf20fe8fd9923739
fantauzd <fantauzd@oregonstate.edu>
2024-04-20 19:00:21 -0400

    Added a brute force solution to change problem

---
+++ change.py
@@ -0,0 +1,24 @@
+# given a list of coin denominations and the needed amount of change,
+# return the least possible number of coins
+
+def makechangeBF(coins, amount):
+    """
+    Brute Force solution to change problem.
+    :param coins: A list of coin denominations like [1, 5, 10, 25]
+    :param amount: The amount of change to return
+    :return: minimum number of coins
+    """
+    # base case is when we have no amount left to return
+    if(amount == 0): return 0
+    result = amount+1 #some arbitary huge number that cannot be the answer
+    # for each coin denomination
+    for i in range(len(coins)):
+        # if that denomination is viable for the amount
+        if(coins[i] <= amount):
+            # find the minimum number of coins to get that amount by checking the
+            # branches for the minimum amount of steps to get each amount after subtracting one coin
+            result = min(result, makechangeBF(coins, amount-coins[i]) + 1)
+    return result
+
+
+print(makechangeBF([1,3,5] , 9 ))
