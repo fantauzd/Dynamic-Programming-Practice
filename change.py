89284d1eb65d1a1627043a93f43d50ce9b22782c
fantauzd <fantauzd@oregonstate.edu>
2024-04-20 19:28:16 -0400

    added memoization to brute force approach

--- change.py
+++ change.py
@@ -20,5 +20,28 @@ def makechangeBF(coins, amount):
             result = min(result, makechangeBF(coins, amount-coins[i]) + 1)
     return result
 
+def makechange(coins, amount, countmemo={}):
+    """
+    Memoization solution to change problem.
+    :param coins: A list of coin denominations like [1, 5, 10, 25]
+    :param amount: The amount of change to return
+    :return: minimum number of coins
+    """
+    # base case is when we have no amount left to return
+    if(amount == 0): return 0
+    result = amount+1 #some arbitary huge number that cannot be the answer
+    if result in countmemo:
+        result = countmemo[amount]
+    else:
+    # for each coin denomination
+        for i in range(len(coins)):
+            # if that denomination is viable for the amount
+            if(coins[i] <= amount):
+                # find the minimum number of coins to get that amount by checking the
+                # branches for the minimum amount of steps to get each amount after subtracting one coin
+                result = min(result, makechangeBF(coins, amount-coins[i]) + 1)
+                countmemo[amount-coins[i]] = result
+    return result
 
-print(makechangeBF([1,3,5] , 9 ))
+if __name__ == "__main__":
+    print(makechange([1,3,5] , 9))
