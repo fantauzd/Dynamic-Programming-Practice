8d6eeda8d405ffa944b6b4e9d8dbf641fb4e7e9e
fantauzd <fantauzd@oregonstate.edu>
2024-04-20 21:06:28 -0400

    added bottom up solution that does not need cent coin

--- change.py
+++ change.py
@@ -78,7 +78,10 @@ def makechange_topdown_helper( coins, amount, countmemo):
     return countmemo[amount]
 
 
-def makechange_bottomup(coins, amount):
+def makechange_bottomup_with_cent(coins, amount):
+    """
+    Assumes there is a coin worth 1.
+    """
     # initialize our countmemo
     count_table = [0] * (amount + 1)
 
@@ -97,10 +100,32 @@ def makechange_bottomup(coins, amount):
     return count_table[amount]
 
 
+def makechange_bottomup(coins, amount):
+    # setting array elements to some large value that is not possible answer
+    min_count_table = [amount + 1] * (amount + 1)
+
+    min_count_table[0] = 0  # setting the base case
+
+    for i in range(1, amount + 1):  # iterate through all possible amount values from base case
+        for j in range(0, len(coins)):  # find the number of coins needed for each coin denomination
+            coin_val = coins[j]
+            # if denomination value is less than amount we are seeking (i) then we can use the coin
+            if (coin_val <= i):
+                # replace min_count_table[i] with minimum value of coins possible
+                min_count_table[i] = min(min_count_table[i], min_count_table[i - coin_val] + 1)
+
+    # we have a valid count of coins if min_count_table[amount] is valid
+    if min_count_table[amount] > amount:
+        result = -1
+    else:
+        result = min_count_table[amount]
+
+    return result
+
 
 if __name__ == "__main__":
-    print(makechange_topdown([1,3,5], 9))
-    print(makechange_bottomup([1,3,5], 9))
+    print(makechange_topdown([1,3,5], 984))
+    print(makechange_bottomup([1,3,5], 984))
 
 
 
