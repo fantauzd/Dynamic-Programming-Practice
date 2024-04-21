786cd92a8753cedb48a998a10b9abeb84e8ad84e
fantauzd <fantauzd@oregonstate.edu>
2024-04-21 19:13:47 -0400

    added bottom up dynamic solution

--- rodCutting.py
+++ rodCutting.py
@@ -19,6 +19,7 @@ def rodCutting_topdown(n, prices):
     vals = [0] * n
     return rodCutting_topdown_helper(n, prices, vals)
 
+
 def rodCutting_topdown_helper(n, prices, vals):
     if n <= 0:
         return 0
@@ -30,5 +31,26 @@ def rodCutting_topdown_helper(n, prices, vals):
         vals[n-1] = max(vals[n-1], rodCutting_topdown_helper(n-i, prices, vals) + prices[i-1])
     return vals[n-1]
 
+
+def rodCutting_bottomup(n, prices):
+    # create an array of size n+1
+    # dp[1] represents the best price for length 1
+    dp = [0 for x in range(n+1)]
+    # starting at 1, solve all subproblems
+    for length in range(1, n+1):
+        max_val = 0
+        # for a rod of length i, we can find what cuts give us the optimal amount
+        for cut in range(length):
+            if((prices[cut] + dp[length-(cut+1)]) > max_val):
+                max_val = prices[cut] + dp[length-(cut+1)]
+
+        dp[length] = max_val
+    # with array built, return answer to length n
+    return dp[n]
+
+
+
+
 if __name__ == "__main__":
     print(rodCutting_topdown(4, [1,5,8,9,10]))
+    print(rodCutting_bottomup(4, [1, 5, 8, 9, 10]))
