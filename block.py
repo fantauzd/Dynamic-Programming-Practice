5ea1684481ab92006ab3a6df8053b3d8dd3f3f09
fantauzd <fantauzd@oregonstate.edu>
2024-04-22 18:18:03 -0400

    added comments

--- block.py
+++ block.py
@@ -11,9 +11,26 @@ def block_puzzle(n):
     # the number of solutions is equal to the sum of number of solutions for the previous 2 lengths
     return block_puzzle(n-1) + block_puzzle(n-2)
 
+def block_puzzle_bottomup(n):
+    # initialize array to store solutions
+    vals = [0] * n
+    #add base cases to solutions array as needed
+    if n >= 1:
+        vals[0] = 1
+    if n >= 2:
+        vals[1] = 2
+    # start from the simplest problem and use previous solutions to solve each subproblem
+    for i in range(3,n+1):
+            vals[i-1] = vals[i-2] + vals[i-3]
+    # return the solution for n from array
+    return vals[n-1]
+
+
+
 if __name__ == "__main__":
-    print(block_puzzle(2))
-    print(block_puzzle(3))
-    print(block_puzzle(4))
-    print(block_puzzle(5))
-    print(block_puzzle(6))
+    print(block_puzzle_bottomup(1))
+    print(block_puzzle_bottomup(2))
+    print(block_puzzle_bottomup(3))
+    print(block_puzzle_bottomup(4))
+    print(block_puzzle_bottomup(5))
+    print(block_puzzle_bottomup(6))
